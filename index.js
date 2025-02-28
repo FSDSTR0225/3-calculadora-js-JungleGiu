681eca651876ff073ddf7ec389dada1b7b23cf71
Giu Eminente <giulemi210@gmail.com>
2025-02-28 17:33:11 +0100

    reset and equal buttons work, canc still not

--- index.js
+++ index.js
@@ -36,85 +36,74 @@ functionsInKeyoard.forEach((fnc) => {
   document.querySelector(".function-buttons").appendChild(functionButton);
 });
 // CREATES ALL THE DOM ELEMENTS
- 
 
 // PINTA EN PANTALLA LOS NUMEROS Y OPERADORES
-const stringOperation = document.querySelector(".display");
+const displayDiv = document.querySelector(".display");
 let firstOperator = document.querySelector("span.data1");
 let secondOperator = document.querySelector("span.data2");
 let operationSymbol = document.querySelector("span.op");
 const addToDisplay = (button) => {
   const buttonValue = button.textContent;
-  if (button.classList.contains("number")&& operationSymbol.textContent) {
-    secondOperator.textContent += button.textContent;}
-  
-    else if (button.classList.contains("operator")) {
-      operationSymbol.textContent += button.textContent; 
-    }
-    else if (operationSymbol.textContent === '') {
-      firstOperator.textContent += button.textContent;
-
+  if (button.classList.contains("number") && operationSymbol.textContent) {
+    secondOperator.textContent += button.textContent;
+  } else if (button.classList.contains("operator")) {
+    operationSymbol.textContent += button.textContent;
+  } else if (operationSymbol.textContent === "") {
+    firstOperator.textContent += button.textContent;
   }
-}
+};
 
 document.querySelectorAll(".number").forEach((numberButton) => {
-  numberButton.addEventListener ('click', () => addToDisplay(numberButton))
+  numberButton.addEventListener("click", () => addToDisplay(numberButton));
 });
 
 document.querySelectorAll(".operator").forEach((operatorButton) => {
-  operatorButton.addEventListener ('click', () => addToDisplay(operatorButton))
+  operatorButton.addEventListener("click", () => addToDisplay(operatorButton));
 });
 
 // PINTA EN PANTALLA LOS NUMEROS Y OPERADORES
 
-
-
 // CALCULA EL RESULTADO Y lo imprime en la pantalla
 
-const calculateExpression = (firstOperator, operationSymbol, secondOperator, ) => {
+const calculateExpression = (
+  firstOperator,
+  operationSymbol,
+  secondOperator
+) => {
   firstOperator = Number(firstOperator);
-  secondOperator = Number(secondOperator); 
+  secondOperator = Number(secondOperator);
   if (operationSymbol === "+") {
-    return  firstOperator + secondOperator;
+    return firstOperator + secondOperator;
   } else if (operationSymbol === "-") {
-    return  firstOperator - secondOperator;
+    return firstOperator - secondOperator;
   } else if (operationSymbol === "*") {
-    return  firstOperator * secondOperator;
+    return firstOperator * secondOperator;
   } else if (operationSymbol === "/") {
-    return  firstOperator / secondOperator;
+    return firstOperator / secondOperator;
   }
-;}
+};
 
-// EVENT LINSTENER PARA LOS BOTONES DE FUNCIONALIDAD 
+// EVENT LINSTENER PARA LOS BOTONES DE FUNCIONALIDAD
 
+document.querySelector(".equal").addEventListener("click", () => {
+  const result = calculateExpression(
+    firstOperator.textContent,
+    operationSymbol.textContent,
+    secondOperator.textContent
+  );
+  const resultDiv = document.createElement("div");
+  resultDiv.classList.add("result");
+  resultDiv.textContent = result;
+  displayDiv.appendChild(resultDiv);
+});
 
-document.querySelector('.equal').addEventListener('click', () => {
-const result =calculateExpression(firstOperator.textContent, operationSymbol.textContent, secondOperator.textContent)
-  const resultDiv =document.createElement('div')
-  resultDiv.classList.add('result')
-  resultDiv.textContent= result
-  stringOperation.appendChild(resultDiv)
-})
+document.querySelector(".reset").addEventListener("click", () => {
+  firstOperator.textContent = "";
+  secondOperator.textContent = "";
+  operationSymbol.textContent = "";
+  document.querySelector(".result").remove();
+});
 
-document.querySelector('.reset').addEventListener('click', () => {
-  firstOperator.textContent = '';
-  secondOperator.textContent ='';
-operationSymbol.textContent= '';
-document.querySelector('.result').textContent= '';
-})
-
-
-const newopString = () => {
-  const spans = document.querySelectorAll('span');
-  let string='';
-  spans.forEach(span =>{string += span.textContent;});
-  return string
-}
-
-
-
-document.querySelector('.canc').addEventListener('click', () =>{
-
-})
+document.querySelector(".canc").addEventListener("click", () => {});
 
 // EVENT LINSTENER PARA LOS BOTONES DE FUNCIONALIDAD
